043486dfb6fa90185f1a4dfb3756e7e348a4d32a
Santatrasoa <santatraherimampionona@gmail.com>
2025-04-28 14:08:12 +0300

    add Shop and Products components; implement Contact component with form and address details; set up main application structure in App component; create ProductController for handling product routes; add footer with subscription form; include logo asset and styles

---
+++ client/src/src/App.tsx
@@ -0,0 +1,16 @@
+import {Header} from "./Header/Header"
+import Contact from "./Contact/Contact"
+import { Footer } from "./Footer/Footer"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
